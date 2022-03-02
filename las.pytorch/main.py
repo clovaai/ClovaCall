8767275518334550f61dc758c9fa2a21a8fb0dd0
ysy2000 <suebook@naver.com>
2022-03-02 20:33:32 +0900

    First commit

--- las.pytorch/main.py
+++ las.pytorch/main.py
@@ -11,6 +11,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+# this paper is for my personal study.
+
 #-*- coding: utf-8 -*-
 
 import os
